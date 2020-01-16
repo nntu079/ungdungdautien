ed17998bdeaba8db18885222b0dd2d92b8823207
nntu079 <46866558+nntu079@users.noreply.github.com>
2020-01-16 17:38:52 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send('Hello con heo mập'));
+app.get('/hoa',(req,res) =>res.send('Hoa rất đẹp'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
